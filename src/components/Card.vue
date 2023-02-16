a4847ed7e859e219c042b6a20d01c1bd60ceff6f
Bohdana Lutska <87516048+bohdanalu@users.noreply.github.com>
2023-02-16 21:03:47 +0200

    Update Card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,7 +3,7 @@
     <RouterLink class="card__link" to="/Course"
       ><img
         class="card__img"
-        :src="catalog_data.img"
+        :src="require(catalog_data.img)"
         :alt="catalog_data.speaker"
     /></RouterLink>
     <div class="card__content">
